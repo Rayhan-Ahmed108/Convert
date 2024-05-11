5e7339c5c90c3f6aa4bf9f5206ca6cc105dd3f5e
Rayhan-Ahmed108 <jeyamxvx12@gmail.com>
2024-05-12 01:04:48 +0600

    rectangle properties

---
+++ src/RectangleProperties.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+public class RectangleProperties {
+    Scanner scanner = new Scanner(System.in);
+    double length = scanner.nextDouble();
+    double width = scanner.nextDouble();
+}
